129442a138d728817568ecf9293ac7df07a56990
Tuxedo21 <Ctps.21@comunidad.unam.mx>
2016-06-22 14:21:30 -0400

    Create kitten botmod

--- index.js
+++ index.js
@@ -27,8 +27,10 @@ app.post('/webhook', function (req, res) {
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
-            sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
-        }
+            if (!kittenMessage(event.sender.id, event.message.text)) {
+              sendMessage(event.sender.id, {text: "Echo: " + event.message.text});
+                                }
+                    }
     }
     res.sendStatus(200);
 });
