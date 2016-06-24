0a15ac666c7712df5d40750cd5af84a00488f17b
Tuxedo21 <Ctps.21@comunidad.unam.mx>
2016-06-24 15:16:23 -0400

    c

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ app.post('/webhook', function (req, res) {
         var event = events[i];
         if (event.message && event.message.text) {
             if (!kittenMessage(event.sender.id, event.message.text)) {
-              //if(!mapMessage(event.sender.id, event.message.text)){
+              if(!mapMessage(event.sender.id, event.message.text)){
                 sendMessage(event.sender.id, {text: "Echo: " + event.message.text + " " +event.postback});
-            //  }
+             }
             }
         } else if (event.postback) {
             console.log("Postback received: " + JSON.stringify(event.postback));
