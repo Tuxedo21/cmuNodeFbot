4d115ddd0e977cdfc2c788f18983cce70291def6
Tuxedo21 <Ctps.21@comunidad.unam.mx>
2016-06-24 11:54:04 -0400

    Create map Bot

--- index.js
+++ index.js
@@ -55,8 +55,6 @@ function sendMessage(recipientId, message) {
         }
     });
 };
-
-
 /*
 values[values.length - 1]
 " 15213"
@@ -77,28 +75,44 @@ function mapMessage(recipientId, inputText){
         mapURL= mapURL.replace(/ /g,"+");
       }
       //print out search
-      message = {
-          "attachment": {
-              "type": "template",
-              "payload": {
-                  "template_type": "generic",
-                  "elements": [{
-                      "title": "Place",
-                      "subtitle": "Event place",
-                      "image_url": mapURL ,
-                      "buttons": [{
-                          "type": "web_url",
-                          "url": mapURL,
-                          "title": "Show kitten"
-                          }, {
-                          "type": "postback",
-                          "title": "I like this",
-                          "payload": "User " + recipientId + " likes kitten " + imageUrl,
-                      }]
+      message: {
+            attachment: {
+              type: "template",
+              payload: {
+                template_type: "generic",
+                elements: [{
+                  title: "rift",
+                  subtitle: "Next-generation virtual reality",
+                  item_url: "https://www.oculus.com/en-us/rift/",
+                  image_url: "http://messengerdemo.parseapp.com/img/rift.png",
+                  buttons: [{
+                    type: "web_url",
+                    url: "https://www.oculus.com/en-us/rift/",
+                    title: "Open Web URL"
+                  }, {
+                    type: "postback",
+                    title: "Call Postback",
+                    payload: "Payload for first bubble",
+                  }],
+                }, {
+                  title: "touch",
+                  subtitle: "Your Hands, Now in VR",
+                  item_url: "https://www.oculus.com/en-us/touch/",
+                  image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+                  buttons: [{
+                    type: "web_url",
+                    url: "https://www.oculus.com/en-us/touch/",
+                    title: "Open Web URL"
+                  }, {
+                    type: "postback",
+                    title: "Call Postback",
+                    payload: "Payload for second bubble",
                   }]
+                }]
               }
+            }
           }
-      };
+
       sendMessage(recipientId, message);
       return true;
     }
