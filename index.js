e454a0122345eb8862190b11f27e212edaacaa10
Tuxedo21 <Ctps.21@comunidad.unam.mx>
2016-06-27 15:28:01 -0400

    dev

--- index.js
+++ index.js
@@ -65,44 +65,44 @@ function mapMessage(recipientId, text){
             mapURL= mapURL.replace(/ /g,"+");
           }
 
-          message: {
-      attachment: {
-        type: "template",
-        payload: {
-          template_type: "generic",
-          elements: [{
-            title: "Is this tha place?" + mapURL,
-            subtitle: "Next-generation virtual reality",
-            item_url: "https://www.oculus.com/en-us/rift/",
-            image_url: "http://messengerdemo.parseapp.com/img/rift.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/rift/",
-              title: "Open Web URL"
+          message = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "Is this tha place?" + mapURL,
+            "subtitle": "Next-generation virtual reality",
+            "item_url": "https://www.oculus.com/en-us/rift/",
+            "image_url": "http://messengerdemo.parseapp.com/img/rift.png",
+            "buttons": [{
+              "type": "web_url",
+              "url": "https://www.oculus.com/en-us/rift/",
+              "title": "Open Web URL"
             }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for first bubble",
+              "type": "postback",
+              "title": "Call Postback",
+              "payload": "Payload for first bubble",
             }],
           }, {
-            title: "touch",
-            subtitle: "Your Hands, Now in VR",
-            item_url: "https://www.oculus.com/en-us/touch/",
-            image_url: "http://messengerdemo.parseapp.com/img/touch.png",
-            buttons: [{
-              type: "web_url",
-              url: "https://www.oculus.com/en-us/touch/",
-              title: "Open Web URL"
+            "title": "touch",
+            "subtitle": "Your Hands, Now in VR",
+            "item_url": "https://www.oculus.com/en-us/touch/",
+            "image_url": "http://messengerdemo.parseapp.com/img/touch.png",
+            "buttons": [{
+              "type": "web_url",
+              "url": "https://www.oculus.com/en-us/touch/",
+              "title": "Open Web URL"
             }, {
-              type: "postback",
-              title: "Call Postback",
-              payload: "Payload for second bubble",
+              "type": "postback",
+              "title": "Call Postback",
+              "payload": "Payload for second bubble",
             }]
           }]
         }
       }
-    }
-  };
+    };
+
             //  print out search
           sendMessage(recipientId, message);
           return true;
