faee1cb55064a3ef844f28c09fd10343d1c86d7c
Tuxedo21 <Ctps.21@comunidad.unam.mx>
2016-07-08 11:38:28 -0400

    battery

--- index.js
+++ index.js
@@ -199,31 +199,39 @@ function volunteerMessage(recipientId, text) {
     }
     return false;
 };
-
 function greetingsMessage(recipientId, text) {
     text = text || "";
     text = text.toLowerCase();
     var values = text.split(' ');
     if (values[0] === 'hello' || values[0] === 'hi' || values[0] === 'hey') {
-            sendMessage(recipientId, {text: "Greetings human, I am the luzDeploy bot. I was created by CMU's HCI team at the biglab! My job is to help you make the world a better place for the handicap. Please tell me which volunteer are you? By writing 'volunteer <number>' (for todays deployment there are only volunteers one two and three)"});
+            sendMessage(recipientId, {text: "Greetings human, I am the luzDeploy bot. I was created by CMU's HCI team at the biglab! My job is to help you make the world a better place for the handicap. Please tell me which volunteer are you? By writing 'volunteer <number>' (for todays deployment there are only volunteers four and five)"});
             return true;
     }
     return false;
 };
 function instructionsMessage(recipientId, text) {
-    if (text === 'one' || text === 'two' || text === 'three' || text === '1' || text === '2' || text === '3') {
+    if (text === 'one' || text === 'two' || text === 'three'
+    || text === '1' || text === '2' || text === '3'
+    || text === '4' || text === '5'
+    || text === 'five' || text === 'four') {
       var message = "";
             if(text === 'three' || text === '3'){
               //three red
               var message = "You will be placing beacons.\n Place a beacon where you see a red square on your map as high as you can and always on the wall.";
             }
             else if(text === 'two' || text === '2'){
-              //two blue
-              var message = "You will be placing beacons.\n Place a beacon where you see a blue square on your map as high as you can and always on the wall.";
+            //two blue
+            var message = "You will be placing beacons.\n Place a beacon where you see a blue square on your map as high as you can and always on the wall.";
             }
             else if(text === 'one' || text === '1'){
             //one pink
             var message = "You will be placing tape on the floor.\nPlace a small peace of tape from the the first beacon in a hallway every meter. Please repeat this for each hallway. For today there are four in total. At the end it should look a little like the map given to you.";
+            }
+            else if(text === 'four' || text === '4'){
+            var message = "I have your instructions human:\n\nThe visually impaired need your help. Your task will be of battery beacon maintenance. These beacons are their eyes. Sometimes the can go out. We need you to make sure they are not out.  The instructions are simple. Use the map provided to find the beacons you will work with. Once you found a beacon please take the beacon down, open it and replace or place a battery in it. Then send me the four digit code on the back of the beacon please. If you need some more help on how to do this, please use the images provided. If their is a beacon missing PLEASE send me a picture of where it should be so i know. If that is not enough, well read it again. And if that still is not enough, don't complain you have a body and a mind! You can figure it out."
+            }
+            else if(text === 'five' || text === '5'){
+            var message = "I have your instructions human:\n\nThe visually impaired need your help. Your task will be of battery beacon maintenance. These beacons are their eyes. Sometimes the can go out. We need you to make sure they are not out.  The instructions are simple. Use the map provided to find the beacons you will work with. Once you found a beacon please take the beacon down, open it and replace or place a battery in it. Then send me the four digit code on the back of the beacon please. If you need some more help on how to do this, please use the images provided. If their is a beacon missing PLEASE send me a picture of where it should be so i know. If that is not enough, well read it again. And if that still is not enough, don't complain you have a body and a mind! You can figure it out."
             }
             sendMessage(recipientId, {text: "Your Instruccions:\n" + message});
             return true;
@@ -236,6 +244,68 @@ function DoneMessage(recipientId, text) {
     var values = text.split(' ');
     if (values[0] === "i'm" || values[1] === 'done' || values[2] === ':)') {
             sendMessage(recipientId, {text: "Thank you very much!\nYou just helped by giving light to the visually impaired.\n\nI am still in research phase, please answer this survey so i can become better at helping.\n\n"+ "https://docs.google.com/forms/d/1hcwB18hnyniWFUQAQDm2MSMdlQQL4QYOG_Md9eFsQnE/viewform"});
+            return true;
+    }
+    return false;
+};
+//WORK ON THISS!!
+function managerMessage(recipientId, text) {
+    text = text || "";
+    text = text.toLowerCase();
+    var values = text.split(' ');
+    if (values[0] === 'volunteer') {
+      var youAre = "You are volunteer ";
+      var blueImageUrl = "https://scontent.xx.fbcdn.net/v/t1.0-9/13533270_10154272438778535_6610747476267727156_n.jpg?oh=2e6fad4cc86cc96781636a1488847e7b&oe=57FCED30";
+      var redImageUrl = "https://scontent.xx.fbcdn.net/v/t1.0-9/13510940_10154272438783535_3809594659337214943_n.jpg?oh=b5d74bbe3c4dcde1ec137bd9ad8bb702&oe=57F1DC9B";
+
+      var sideImageUrl = "https://scontent.xx.fbcdn.net/v/t1.0-9/13592396_10154287796748535_4137404384532504939_n.jpg?oh=55be5271fb2922f90455bfde572c43fd&oe=5832ED41"
+      var explodeImageUrl = "https://scontent.xx.fbcdn.net/v/t1.0-9/13592318_10154287796758535_3022623960114472833_n.jpg?oh=e9ce0e7bd48efc35268e01eaa074e500&oe=57F42F0D"
+      var nailImageUrl = "https://scontent.xx.fbcdn.net/v/t1.0-9/13438974_10154287796753535_6660619595394208385_n.jpg?oh=7508738851ca3a9705f71b15b9eecf0a&oe=57F0FAE7"
+      var videoUrl = "https://www.youtube.com/watch?v=1d-Wxf1b55o"
+
+      if (values[1] === 'five' || values[1] === '5'){
+          var imageUrl = blueImageUrl;
+      }else if (values[1] === 'four' || values[1] === '4') {
+          var imageUrl = redImageUrl;
+      }
+            message = {
+                "attachment": {
+                    "type": "template",
+                    "payload": {
+                        "template_type": "generic",
+                        "elements": [{
+                            "title": "Work Map",
+                            "subtitle": youAre + values[1] + ", your tasks today are part of beacon deployment.",
+                            "image_url": imageUrl ,
+                            "buttons": [{
+                                "type": "web_url",
+                                "url": imageUrl,
+                                "title": "Show Image"
+                                }, {
+                                "type": "postback",
+                                "title": "Write: I'm done :)",
+                                "payload": "User " + recipientId + " likes kitten " + imageUrl,
+                            }]
+                        },{
+                        title: "touch",
+                        subtitle: "Your Hands, Now in VR",
+                        item_url: "https://www.oculus.com/en-us/touch/",
+                        image_url: "http://messengerdemo.parseapp.com/img/touch.png",
+                        buttons: [{
+                          type: "web_url",
+                          url: "https://www.oculus.com/en-us/touch/",
+                          title: "Open Web URL"
+                        }, {
+                          type: "postback",
+                          title: "Call Postback",
+                          payload: "Payload for second bubble",
+                        }
+                      }           ]
+                    }
+                }
+            };
+            sendMessage(recipientId, message);
+            instructionsMessage(recipientId,values[1]);
             return true;
     }
     return false;
