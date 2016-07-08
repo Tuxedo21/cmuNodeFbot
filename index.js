2f3418b5d88df1cf3c90ae51828501e862db8573
Tuxedo21 <Ctps.21@comunidad.unam.mx>
2016-07-08 11:59:58 -0400

    battery

--- index.js
+++ index.js
@@ -234,6 +234,7 @@ function instructionsMessage(recipientId, text) {
             else if(text === 'five' || text === '5'){
             var message = "I have your instructions human:\n\nThe visually impaired need your help. Your task will be of battery beacon maintenance. These beacons are their eyes. Sometimes the can go out. We need you to make sure they are not out.  The instructions are simple. Use the map provided to find the beacons you will work with. Once you found a beacon please take the beacon down, open it and replace or place a battery in it. Then send me the four digit code on the back of the beacon please. If you need some more help on how to do this, please use the images provided. If their is a beacon missing PLEASE send me a picture of where it should be so i know. If that is not enough, well read it again. And if that still is not enough, don't complain you have a body and a mind! You can figure it out."
             }
+
             sendMessage(recipientId, {text: "Your Instruccions:\n" + message});
             return true;
     }
