cd6f065932f5eed437c42c2384dcb5cff2fc2235
Tuxedo21 <Ctps.21@comunidad.unam.mx>
2016-07-18 15:22:40 -0400

    instructions

--- index.js
+++ index.js
@@ -78,7 +78,7 @@ function startASMessage(recipientId, text){
             // startas, 1, 120, 3, 5
           for (var i = 0; i < values[4]; i++) {
             sendMessage(ids.idArray[i], {text: "Hello volunteer: " + (i +1) + "\nInstructions..."});
-            batteryImageMessage(ids.idArray[i]);
+            batteryMessage(ids.idArray[i]);
             //  SEND INSTRUCTIONS
           }
 
