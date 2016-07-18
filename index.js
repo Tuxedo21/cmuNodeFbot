85a828818597594723a9dfdf6690501623ec3610
Tuxedo21 <Ctps.21@comunidad.unam.mx>
2016-07-18 01:28:52 -0400

    Working on migration

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.post('/webhook', function (req, res) {
         if (event.message && event.message.text) {
             if (!kittenMessage(event.sender.id, event.message.text)){
 
-                //volunteerEventMessage(event.sender.id, event.message.text);
+                volunteerEventMessage(event.sender.id, event.message.text);
                 sendMessage(event.sender.id, {text: "For debugging echo: " + event.message.text + "\n Id: " + event.sender.id});
               if(event.sender.id == ids.carlId){
                   startASMessage(event.sender.id, event.message.text);
