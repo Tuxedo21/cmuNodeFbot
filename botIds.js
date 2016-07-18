c4e10ed7bd3b5911fd58a1bbddf9905d6f15b635
Tuxedo21 <Ctps.21@comunidad.unam.mx>
2016-07-17 20:59:32 -0400

    AS is done

--- botIds.js
+++ botIds.js
@@ -4,9 +4,11 @@ module.exports = function Ids() {
     this.joshId = 1224225177608747;
     this.jessId = 1052157704870468;
     //Dummy Accounts
-    this.beniId = 872644172839875; //Benito Camelo 
-    this.bajId = 935052293271240; //Bjarne Thompson
+    this.beniId = 872644172839875; //Benito Camelo
+    this.bajId = 935052293271240;  //Bjarne Thompson
     this.estId = 1131034940276348; //Esteban Hernández
     this.zamId = 1205708212783299; //Luis Zamudio Rodríguez
-    this.jorId = 907551349356945; //Jorge Luis Ortega Sánchez
+    this.jorId = 907551349356945;  //Jorge Luis Ortega Sánchez
+    this.marId = 1047105345325390; //Margarita Blanca pinepowertech
+    this.celId = 1047978891957490; //Celestial Azul pinepowertech
 }
