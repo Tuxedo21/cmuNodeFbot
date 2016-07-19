5f59136656c047c16406c825d962bb40bea58043
Tuxedo21 <Ctps.21@comunidad.unam.mx>
2016-07-19 11:53:21 -0400

    starting AS

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ app.post('/webhook', function (req, res) {
     for (i = 0; i < events.length; i++) {
         var event = events[i];
         if (event.message && event.message.text) {
-          g = g + 1;
+          g = g + 1 ;
             if (!kittenMessage(event.sender.id, event.message.text) || !mapMessage(event.sender.id, event.message.text)){
                 volunteerEventMessage(event.sender.id, event.message.text);
                 sendMessage(event.sender.id, {text: g + " For debugging echo: " + event.message.text + "\n Id:" + event.sender.id + "\n Time:" +algoVE.getCurrentTime()});
