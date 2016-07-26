f1ab5644210a81c9a7935ff48bda1d07e72a8e02
Tuxedo21 <Ctps.21@comunidad.unam.mx>
2016-07-26 15:06:54 -0400

    Working on json

--- index.js
+++ index.js
@@ -210,7 +210,7 @@ function volunteerEventMessage(recipientId, text){
               globalVolTaskArray[volIndex].push(globalTaskArray.pop());
 
               //Send new task
-              // sendMessage(recipientId, {text: "Your task should take: " + "[" + globalVolTaskArray[i][0][0] + "] minutes." });
+              //sendMessage(recipientId, {text: "Your task should take: " + "[" + globalVolTaskArray[i][0][0] + "] minutes." });
               // sendInstructions(globalVolTaskArray[volIndex][0][1],recipientId);
               //                 sendMessage(recipientId, {text: "time::[" + globalVolTaskArray[i][0][0] + "]::" });
               //                 sendMessage(recipientId, {text: "type::[" + globalVolTaskArray[i][0][1] + "]::" });
