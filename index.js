9c9d49859d9a4c01c05139a9f66708115d18581c
Tuxedo21 <Ctps.21@comunidad.unam.mx>
2016-07-26 14:26:28 -0400

    Working on json

--- index.js
+++ index.js
@@ -182,8 +182,8 @@ function volunteerEventMessage(recipientId, text){
 
            jsonContent.workPool = jsonContent.workPool - 1;// take away from the pool
            fs.writeFileSync("botData.json", JSON.stringify(jsonContent)); //update the json
-            sendMessage(recipientId, {text: "debugging::[" + jsonContent.workPool + "]::" });
-//           globalDoneTime[volIndex] = Number(algoVE.getCurrentTime()); //get done time
+           globalDoneTime[volIndex] = Number(algoVE.getCurrentTime()); //get done time
+                       sendMessage(recipientId, {text: "debugging::[" + globalDoneTime[volIndex] + "]::" });
 //
 //            if(globalVolTaskArray[volIndex].length != 0) {
 //               var xi =  globalVolTaskArray[volIndex][0][0] / (globalDoneTime[volIndex] - globalStartTime[volIndex]); //xi for weight
